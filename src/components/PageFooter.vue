ca485849760d1c2e4ee0f96254b79cc028aa38d8
Samuel Scozzari <s_scozzari@libero.it>
2022-01-27 16:32:13 +0100

    PageFooter.vue structure revisited

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <footer>
+    <!-- Top Footer -->
+    <top-footer />
+    <!-- Mid Footer -->
+    <mid-footer />
+    <!-- Spam Section -->
+    <spam-section />
+  </footer>
 </template>
 
 <script>
-export default {};
+import TopFooter from "./footer/TopFooter.vue";
+import MidFooter from "./footer/MidFooter.vue";
+import SpamSection from "./footer/SpamSection.vue";
+
+export default {
+  components: { SpamSection },
+  TopFooter,
+  MidFooter,
+  SpamSection,
+};
 </script>
 
 <style lang="scss" scoped></style>
