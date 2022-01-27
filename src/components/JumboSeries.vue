101858f22793ec6695f4585445ac82251feb2e84
Samuel Scozzari <s_scozzari@libero.it>
2022-01-27 16:23:20 +0100

    created and added main Jumbotron section

---
+++ src/components/JumboSeries.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="teen-t-jumbo"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.teen-t-jumbo {
+  height: 400px;
+  background-image: url("../assets/img/jumbotron.jpg");
+  background-size: cover;
+}
+</style>
