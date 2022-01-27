ef3fd5ecc0574036790a312cfe89e326fa643159
Samuel Scozzari <s_scozzari@libero.it>
2022-01-27 16:07:07 +0100

    main section base structure completed

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,1 +1,14 @@
-<style lang="scss" scoped></style>
+<template>
+  <div black-bar></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+div {
+  background-color: #1c1c1c;
+  height: 130px;
+}
+</style>
