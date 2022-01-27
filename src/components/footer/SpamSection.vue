0cc84efc4f50c7c15b58c195e412999ee427fc23
Samuel Scozzari <s_scozzari@libero.it>
2022-01-27 18:32:09 +0100

    SpamSection.vue structure, EXERCISE COMPLETED

--- src/components/footer/SpamSection.vue
+++ src/components/footer/SpamSection.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="spam-cont"></div>
+  <div class="spam-cont">
+    <input type="button" value="SIGN-UP NOW!" />
+    <nav>
+      <h3>FOLLOW US</h3>
+      <ul>
+        <li>
+          <img src="@/assets/img/footer-facebook.png" alt="" />
+        </li>
+        <li>
+          <img src="@/assets/img/footer-twitter.png" alt="" />
+        </li>
+        <li>
+          <img src="@/assets/img/footer-youtube.png" alt="" />
+        </li>
+        <li>
+          <img src="@/assets/img/footer-pinterest.png" alt="" />
+        </li>
+        <li>
+          <img src="@/assets/img/footer-periscope.png" alt="" />
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -7,10 +29,41 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "@/style/var.scss";
+
 .spam-cont {
   height: 100px;
   background-color: #303030;
   display: flex;
   position: relative;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 270px;
+
+  input {
+    background-color: transparent;
+    height: 50px;
+    width: 150px;
+    font-size: 16px;
+    font-weight: 600;
+    border: 2px solid $dc-blue;
+    color: white;
+  }
+
+  nav {
+    display: $display-f;
+    align-items: center;
+
+    h3 {
+      color: $dc-blue;
+      margin-right: 30px;
+    }
+
+    ul {
+      li {
+        margin-right: 20px;
+      }
+    }
+  }
 }
 </style>
